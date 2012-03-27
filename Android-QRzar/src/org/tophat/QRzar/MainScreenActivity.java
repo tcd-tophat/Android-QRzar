9124b72b35219a516cd5864ddf71a06b347eb9dc
Kevin Bluett <kevin.bluett@gmail.com>
2012-03-27 21:26:11 +0100

    Added Fixes.

--- Android-QRzar/src/org/tophat/QRzar/MainScreenActivity.java
+++ Android-QRzar/src/org/tophat/QRzar/MainScreenActivity.java
@@ -1,19 +1,28 @@
 package org.tophat.QRzar;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainScreenActivity extends Activity{
 	
 	public final static int SCAN_RESULT = 1;
+	
+	public static Context c;
 	
 	 /* Control the main screen of the app. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainscreen);
+        
+        c = this.getApplicationContext();
     }
     
     @Override
@@ -45,4 +54,33 @@ public class MainScreenActivity extends Activity{
 		startActivityForResult(intent, SCAN_RESULT);
 		setContentView(R.layout.loadingscreen);
     }
+    
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    	  if (intent != null) {
+    		  String response = intent.getStringExtra("SCAN_RESULT");
+    		  setContentView(R.layout.mainscreen);
+    		  showNotification("data" + response );
+    	  }
+    }
+    
+	/**
+	 * Makes Toast twice as fast, without burning!
+	 * @param data
+	 */
+	public static void showNotification(String data)
+	{
+		MainScreenActivity.showNotification(data, Toast.LENGTH_SHORT);
+	}
+
+	/**
+	 * Shows the notification for a custom defined period.
+	 * @param data
+	 * @param duration
+	 */
+	public static void showNotification(String data, int duration)
+	{
+		Toast toast;
+		toast = Toast.makeText(MainScreenActivity.c, data, duration);
+		toast.show();
+	}
 }
