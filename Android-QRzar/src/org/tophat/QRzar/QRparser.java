69f04c17892b933859e8374c79c3a22870e7316d
Kevin Bluett <kevin.bluett@gmail.com>
2012-03-31 18:21:35 +0100

    Fixed a number of bugs in the QRparser that would stop it from compiling.

--- Android-QRzar/src/org/tophat/QRzar/QRparser.java
+++ Android-QRzar/src/org/tophat/QRzar/QRparser.java
@@ -1,45 +1,61 @@
 package org.tophat.QRzar;
 
-import java.Math;
-
-public class QRparser{
+/**
+ * Parses the team join QR code in order to the user to join a specific game.
+ * @author Kevin
+ *
+ */
+public class QRparser
+{
 
     private String data = "";
 
-    public QRparser(String QRinput){
-        data = QRinput.clone();   
+    public QRparser(String QRinput)
+    {
+    	//Clone the string
+        data = new String ( QRinput);   
     }
 
-    public String getTeam(){
-        if(data.charAt(0) == 'R'){
+    public String getTeam()
+    {
+    	
+        if(data.charAt(0) == 'R')
+        {
             return "red";
         }
-        if(data.charAt(0) == 'B'){
+        else if(data.charAt(0) == 'B')
+        {
             return "blue";
         }
-        if(data.charAt(0) == 'G'){
+        else if(data.charAt(0) == 'G')
+        {
             return "green";
         }
-        if(data.charAt(0) == 'Y'){
+        else if(data.charAt(0) == 'Y')
+        {
             return "yellow";
         }
+        
+		return null;
     }
 
-    public int getGameId(){
+    public int getGameId()
+    {
         int res = 0;
         res += (int)data.charAt(1);
-        res << Math.pow(2, 8);
+        res = res << (int) Math.pow( (double) 2, (double)8);
         res += (int)data.charAt(2);
-        res << Math.pow(2, 8);
+        res = res << (int) Math.pow( (double)2, (double)8);
         res += (int)data.charAt(3);
         return res;
     }
 
-    public int getPlayerId(){
+    public int getPlayerId()
+    {
         int res = 0;
         res += (int)data.charAt(4);
-        res << Math.pow(2, 8);
-        res += (int)data.chatAt(5);
+        res = res << (int)Math.pow((double)2, (double)8);
+        res += (int)data.charAt(5);
         return res;
     }
 }
