44e08640d1da93db25e37cea680285846db0d252
Kevin Bluett <kevin.bluett@gmail.com>
2012-08-21 16:50:38 +0100

    Refactored a file & removed an unnecessary file

--- Android-QRzar/src/org/tophat/QRzar/SignInActivity.java
+++ Android-QRzar/src/org/tophat/QRzar/SignInActivity.java
@@ -5,47 +5,59 @@ import android.content.Intent;
 import android.os.Bundle;
 
 /* Handle all signing in work */
-public class SignInActivity extends Activity {
+public class SignInActivity extends Activity 
+{
 	
 	private int authToken = 0;
 	
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
     
     @Override
-    public void onStart(){
+    public void onStart()
+    {
     	super.onStart();
     }
     
     @Override
-    public void onResume(){
+    public void onResume()
+    {
     	super.onResume();
-    	if(isSignedIn()){		//If the player already has an auth token
+    	
+    	if(isSignedIn())
+    	{		//If the player already has an auth token
     		Intent signInIntent = new Intent(this, MainScreenActivity.class);
     		startActivity(signInIntent);
     	}
-    	else{
-    		if(signIn()){
+    	else
+    	{
+    		if(signIn())
+    		{
     			//Player has been signed in
     		}
-    		else{
+    		else
+    		{
     			//Handle login errors
     		}
     	}
     }
     
-    public boolean isSignedIn(){
-    	if(authToken != 0){
+    public boolean isSignedIn()
+    {
+    	if(authToken != 0)
+    	{
     		return true;
     	}
     	return false;
     }
     
-    public boolean signIn(){
-    /*Sign in the player and set suthToken */
+    public boolean signIn()
+    {
+    	/*Sign in the player and set suthToken */
     	authToken = 1;
     	return true;
     }
